8489f839feab6125bdfe457dcfe2771d65c80058
Max16 <max16@naver.com>
2021-06-09 15:37:04 +0900

    #13.6 AppLoading

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
+import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  // AppLoading part
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false); // loading 이 끝나면 실행
+  const preload = () => {
+    const fontToLoad = [Ionicons.font]; // 사용할 icon.font 파일 로드
+    const fontPromises = fontToLoad.map((font) => Font.loadAsync(font)); // Array 형태의 promise 로 return
+    return Promise.all(fontPromises);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Hello</Text>
