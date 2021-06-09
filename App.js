21138c7abdfdfc45ba36347df45203848c66727d
Max16 <max16@naver.com>
2021-06-09 16:18:33 +0900

    13.7 AppLoading part Two

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
@@ -10,9 +11,11 @@ export default function App() {
   const [loading, setLoading] = useState(true);
   const onFinish = () => setLoading(false); // loading 이 끝나면 실행
   const preload = () => {
-    const fontToLoad = [Ionicons.font]; // 사용할 icon.font 파일 로드
-    const fontPromises = fontToLoad.map((font) => Font.loadAsync(font)); // Array 형태의 promise 로 return
-    return Promise.all(fontPromises);
+    const fontsToLoad = [Ionicons.font]; // 사용할 icon.font 파일 로드
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font)); // Array 형태의 promise 로 return
+    const imagesToLoad = [require("./assets/logo.png")];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
   };
   if (loading) {
     return (
