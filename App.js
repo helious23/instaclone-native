63d921261ce119de3ab081e090b8658ae32b5f3c
Max16 <max16@naver.com>
2021-06-11 08:07:37 +0900

    #14.1 Moving Through Screens

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import AppLoading from "expo-app-loading";
-import { StatusBar } from "expo-status-bar";
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { LoggedOutNav } from "./navigators/LoggedOutNav";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   // AppLoading part
@@ -27,18 +27,8 @@ export default function App() {
     );
   }
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/CreateAccount.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+export const CreateAccount = () => {
+  return (
+    <View>
+      <Text>Create Account</Text>
+    </View>
+  );
+};
