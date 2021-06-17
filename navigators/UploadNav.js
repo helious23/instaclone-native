28d4f8cd835401bb5329fb914217c4d5dc2814d4
Max16 <max16@naver.com>
2021-06-17 09:11:01 +0900

    #17.2 Select Photo part One

--- navigators/UploadNav.js
+++ navigators/UploadNav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import SelectPhoto from "../screens/SelectPhoto";
 import TakePhoto from "../screens/TakePhoto";
@@ -23,8 +24,24 @@ export default function UploadNav() {
     >
       <Tab.Screen name="Select">
         {() => (
-          <Stack.Navigator>
-            <Stack.Screen name="Select" component={SelectPhoto} />
+          <Stack.Navigator
+            screenOptions={{
+              headerTintColor: theme === "dark" ? "#fff" : "#000",
+              headerBackTitleVisible: false,
+              headerBackImage: ({ tintColor }) => (
+                <Ionicons color={tintColor} name="close" size={28} />
+              ),
+              headerStyle: {
+                backgroundColor: theme === "dark" ? "#000" : "#fff",
+                shadowOpacity: 0.3,
+              },
+            }}
+          >
+            <Stack.Screen
+              name="Select"
+              options={{ title: "Choose a photo" }}
+              component={SelectPhoto}
+            />
           </Stack.Navigator>
         )}
       </Tab.Screen>
