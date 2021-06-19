80f39ee9b02a405073e0354a199a3b83446c0e83
Max16 <max16@naver.com>
2021-06-19 12:46:10 +0900

    #17.13 Upload Screen part Two

--- screens/UploadForm.js
+++ screens/UploadForm.js
@@ -1,11 +1,75 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { ActivityIndicator, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import DismissKeyboard from "../components/DismissKeyboard";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${(props) => props.theme.bgColor};
+  padding: 0px 50px;
+`;
+const Photo = styled.Image`
+  height: 350px;
+`;
+const CaptionContainer = styled.View`
+  margin-top: 30px;
+`;
+const Caption = styled.TextInput`
+  background-color: ${(props) => props.theme.fontColor};
+  color: ${(props) => props.theme.bgColor};
+  padding: 10px 20px;
+  border-radius: 100px;
+`;
+
+const HeaderRightText = styled.Text`
+  color: ${(props) => props.theme.blue};
+  font-size: 16px;
+  font-weight: 600;
+  margin-right: 10px;
+`;
 
 export default function UploadForm({ route, navigation }) {
-  console.log(route);
+  const HeaderRightLoading = () => (
+    <ActivityIndicator size="small" color="#fff" style={{ marginRight: 10 }} />
+  );
+  const { register, handleSubmit, setValue } = useForm();
+  const HeaderRight = () => (
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("UploadForm", {
+          file: chosenPhoto,
+        })
+      }
+    >
+      <HeaderRightText>Next</HeaderRightText>
+    </TouchableOpacity>
+  );
+  useEffect(() => {
+    register("caption");
+  }, [register]);
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: HeaderRightLoading,
+      headerLeft: () => null,
+    });
+  }, []);
+  const onValid = ({ caption }) => {};
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <DismissKeyboard>
+      <Container>
+        <Photo resizeMode="contain" source={{ uri: route?.params?.file }} />
+        <CaptionContainer>
+          <Caption
+            placeholder="Write a caption..."
+            placeholderTextColor="rgba(0, 0, 0, 0.5)"
+            onChangeText={(text) => setValue("caption", text)}
+            onSubmitEditing={handleSubmit(onValid)}
+            returnKeyType="done"
+          />
+        </CaptionContainer>
+      </Container>
+    </DismissKeyboard>
   );
 }
